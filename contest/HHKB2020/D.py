716632f409fd1292d88d671f06f5c31905025846
TakahiroSono <taka.sono889@gmail.com>
2020-10-13 16:22:40 +0900

    try contest HHKB2020 D.py

--- contest/HHKB2020/D.py
+++ contest/HHKB2020/D.py
@@ -3,6 +3,8 @@ T = int(input())
 
 for _ in range(T):
   n, a, b = map(int, input().split())
+  # a >= b
+  a, b = max(a, b), min(a, b)
   # Aを敷き詰める
   a_max = (n - a + 1) ** 2
   # Bを敷き詰める
@@ -10,6 +12,9 @@ for _ in range(T):
   # 敷き詰める重複あり
   all_max = a_max * b_max
   # 重複
-  dup = (a + b - 1) ** 2
+  ## 角
+  dup_sq = 4 * ((a - b + 1) ** 2)
+
+  dup = () * (a + b - 1) ** 2
   print(a_max, b_max, dup)
   print((all_max - dup) % MOD)
