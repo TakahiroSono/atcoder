9fbf7f442c41ba4d96b4b9190f090db618a7bac0
TakahiroSono <taka.sono889@gmail.com>
2021-06-10 19:05:08 +0900

    practice ABC203D

--- practice/python/ABC203/D.py
+++ practice/python/ABC203/D.py
@@ -1,14 +1,25 @@
 N, K = map(int, input().split())
 
 A = [list(map(int, input().split())) for _ in range(N)]
-med = []
-for i in range(N-K+1):
-  for j in range(N-K+1):
-    if i+K > N or j+K > N: continue
-    a_map = sum([a[j:j+K] for a in A[i:i+K]], [])
-    a_map.sort()
-    print(i,j,a_map)
-    med.append(a_map[K**2 // 2 - 1 + bool(K**2 % 2)])
+S = [[0] * (N + 1)for _ in range(N + 1)]
 
-print(min(med))
-print(med)
+ng = -1
+ok = 10**9
+lim = (K*K) // 2 + 1
+
+while ng + 1 < ok:
+  x = (ng + ok) // 2;
+  for i in range(1, N + 1):
+    for j in range(1, N + 1):
+      S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1]
+      if A[i - 1][j - 1] > x: S[i][j] += 1
+  ext = False
+
+  for i in range(N - K + 1):
+    for j in range(N - K + 1):
+      if S[i + K][j + K] + S[i][j] - S[i][j + K] - S[i + K][j] < lim:
+        ext = True
+  if ext: ok = x
+  else: ng = x
+
+print(ok)
